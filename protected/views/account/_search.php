9a0f55ff90ecbd7ded89da265906a4f525bcf144
Dave <davey.glenn@gmail.com>
2013-06-15 17:47:26 +0100

    initial commit Yii + Bootstrap + basic models

---
+++ protected/views/account/_search.php
@@ -0,0 +1,32 @@
+<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<?php echo $form->textFieldRow($model,'id',array('class'=>'span5')); ?>
+
+	<?php echo $form->textFieldRow($model,'user_id',array('class'=>'span5')); ?>
+
+	<?php echo $form->textFieldRow($model,'acc_name',array('class'=>'span5','maxlength'=>100)); ?>
+
+	<?php echo $form->textFieldRow($model,'acc_type',array('class'=>'span5','maxlength'=>100)); ?>
+
+	<?php echo $form->textFieldRow($model,'acc_balance',array('class'=>'span5','maxlength'=>10)); ?>
+
+	<?php echo $form->textFieldRow($model,'create_time',array('class'=>'span5')); ?>
+
+	<?php echo $form->textFieldRow($model,'create_user_id',array('class'=>'span5')); ?>
+
+	<?php echo $form->textFieldRow($model,'update_time',array('class'=>'span5')); ?>
+
+	<?php echo $form->textFieldRow($model,'update_user_id',array('class'=>'span5')); ?>
+
+	<div class="form-actions">
+		<?php $this->widget('bootstrap.widgets.TbButton', array(
+			'buttonType'=>'submit',
+			'type'=>'primary',
+			'label'=>'Search',
+		)); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
